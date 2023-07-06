d0780301c20ea364c0b5fabdab53f4294d4fbc51
DarikMasel <darikmasel@mail.ru>
2023-07-06 22:47:26 +0300

    2.16

--- api.js
+++ api.js
@@ -1,13 +1,16 @@
-import { newName, newComment, addButton } from "./comments.js";
-import { commentClickListener } from "./main.js";
-import { renderComments, renderLoaderComments, renderForm } from "./renderComments.js";
+// import { newName , newComment} from "./comments.js";
+import { listenersOfForm} from "./listeners.js";
+import { renderComments, renderLoaderComments, renderForm, renderInputs, canLogined } from "./renderComments.js";
+import { cleareInputs} from "./utilis.js";
 
-let loadedComment = true
+
+// let logined = false
 let allComments = []
+let userData = []
 
-function getComments () {
+function getComments (userData) {
   renderLoaderComments()
-  return fetch("https://webdev-hw-api.vercel.app/api/v1/darikmasel/comments", {
+  return fetch("https://wedev-api.sky.pro/api/v2/darikmasel/comments", {
     method: "GET",
   }).then((response) => {
     if (response.status === 200) {
@@ -27,21 +30,25 @@ function getComments () {
         isLiked: comment.isLiked,
       }
     })
-    loadedComment = false
+    let loadedComment = false
     renderForm(loadedComment)
     renderComments();
+      renderInputs()
   }).catch((error) => {
     console.log(error)
     alert(error)
-    loadedComment = false
+    let loadedComment = false
     renderForm(loadedComment)
   });
 }
 getComments()
 
-function postComments () {
-  return fetch("https://webdev-hw-api.vercel.app/api/v1/darikmasel/comments", {
+function postComments (newComment) {
+  return fetch("https://wedev-api.sky.pro/api/v2/darikmasel/comments", {
     method: "POST",
+    headers: {
+      Authorization: 'Bearer ' + userData.user.token
+    },
     body: JSON.stringify({
       forceError: false,
       "text": newComment.value.replaceAll("&", "&amp;")
@@ -50,14 +57,11 @@ function postComments () {
             .replaceAll('"', "&quot;")
             .replace("|", "<div class='quote'>")
             .replace("|", "</div>"),
-      "name": newName.value.replaceAll("&", "&amp;")
-            .replaceAll("<", "&lt;")
-            .replaceAll(">", "&gt;")
-            .replaceAll('"', "&quot;"),
+      "name": userData.user.name,
     })}).then((response) => {
       if (response.status === 201) {
         return response.json()
-      } if (response.status === 400 || newComment.value.length < 3 && newName.value.length < 3) {
+      } if (response.status === 400 ) {
         return Promise.reject(new Error("Имя или коммент слишком короткие"))
       } if (response.status === 500) {
         return Promise.reject(new Error("Сервер упал"))
@@ -67,10 +71,11 @@ function postComments () {
       }
     }).then((responseData) => {
       getComments()
-      cleareInputs()
+      cleareInputs(newComment)
         renderComments();
+          renderInputs()
     }).catch((error) => {
-      loadedComment = false
+      let loadedComment = false
       renderForm(loadedComment)
       if (error.message === "Сервер упал") {
         postComments()
@@ -82,22 +87,81 @@ function postComments () {
     });
 }
 
-
-
- function addNewComment() {
-      let date = new Date();
-      loadedComment = true
+export function loginUser (regLogin, regName, regPassword) {
+  return fetch("https://wedev-api.sky.pro/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      forceError: false,
+      "login": regLogin.value,
+  "name": regName.value,
+  "password": regPassword.value
+    })}).then((response) => {
+      if (response.status === 201) {
+        return response.json()
+      } if (response.status === 400 ) {
+        return Promise.reject(new Error("Пользователь с таким логином существует"))
+      } if (response.status === 500) {
+        return Promise.reject(new Error("Сервер упал"))
+      } else {
+        console.log(response.status)
+        return Promise.reject(new Error("неизвестная ошибка"))
+      }
+    }).then((responseData) => {
+      getComments()
+      userData = responseData
+      canLogined(userData.user.token)
+      listenersOfForm()
+      return userData
+      
+    }).catch((error) => {
+      let loadedComment = false
       renderForm(loadedComment)
-      postComments()
-      renderComments()
-      commentClickListener()
+      if (error.message === "Сервер упал") {
+        postComments()
+      } if (error.message === 'Failed to fetch') {
+        alert('Нет соединения, попробуйте позже')
+      } else {
+        alert(error.message)
+      }
+    });
+    
 }
 
-export {addNewComment, allComments}
-
-
-function cleareInputs () {
-  newName.value = ''
-  newComment.value = ''
-  addButton.setAttribute('disabled', 'disabled')
+export function autorisationUser (logLogin, logPassword) {
+  return fetch("https://wedev-api.sky.pro/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      forceError: false,
+      "login": logLogin.value,
+  "password": logPassword.value
+    })}).then((response) => {
+      if (response.status === 201) {
+        return response.json()
+      } if (response.status === 400 ) {
+        return Promise.reject(new Error("неверный логин и/или пароль"))
+      } if (response.status === 500) {
+        return Promise.reject(new Error("Сервер упал"))
+      } else {
+        console.log(response.status)
+        return Promise.reject(new Error("неизвестная ошибка"))
+      }
+    }).then((responseData) => {
+      getComments()
+      userData = responseData
+      canLogined(userData.user.token)
+      listenersOfForm()
+      return userData
+    }).catch((error) => {
+      let loadedComment = false
+      renderForm(loadedComment)
+      if (error.message === "Сервер упал") {
+        postComments()
+      } if (error.message === 'Failed to fetch') {
+        alert('Кажеться у вас сломался интернет, попробуйте позже')
+      } else {
+        console.log(error.message)
+      }
+    });
 }
+
+export {allComments, postComments, userData}
---
+++ listeners.js
@@ -0,0 +1,108 @@
+import { form, newName, newComment, addButton} from "./comments.js";
+import {allComments, autorisationUser, loginUser, userData } from "./api.js";
+import { AddLikeOrDelLike } from "./utilis.js";
+import { addNewComment } from "./utilis.js";
+import {  renderRegForm, autorizationForm} from "./renderComments.js";
+
+let commentClickListener = () => {
+
+  let boxOfCommentsTexts = document.querySelectorAll('.comment')
+
+  for (let comment of boxOfCommentsTexts) {
+    comment.addEventListener('click', () => {
+      newComment.setAttribute('style', 'white-space: pre-line;');
+      let replace = `${allComments[comment.dataset.id].text} \r\n \r\n ${allComments[comment.dataset.id].name}`
+      newComment.value = `| ${replace} \r\n\|`
+    })
+  }
+}
+let initEventListeners = () => {
+let likeButtons = document.querySelectorAll('.likes');
+
+  for (let likeButton of likeButtons) {
+    likeButton.addEventListener('click', (e) => {
+    e.stopPropagation()
+    AddLikeOrDelLike(e)
+  })
+  
+}
+}
+let comments = document.querySelector('.comments');
+
+export function listenersOfForm() {
+  let addButton = document.querySelector('.add-form-button')
+  let form = document.querySelector('.add-form')
+
+
+  addButton.addEventListener('click', addNewComment)
+
+  form.addEventListener('keyup', function (event) {
+      if (!addButton.attributes.disabled && event.code == 'Enter') {
+         addNewComment()
+      }
+      else{
+          return
+      }
+
+})
+let delButton = document.querySelector('.del-form-button');
+delButton.addEventListener('click', function () {
+  comments.removeChild(comments.lastChild)
+  allComments.pop()
+})
+}
+
+function listenerHref() {
+  let href = document.getElementById("reg-href")
+  href.addEventListener('click', function (event) {
+    event.preventDefault()
+    renderRegForm()
+    listenerOfReg()
+  })
+}
+
+function listenerOfReg() {
+let regButton = document.getElementById('reg-button')
+let regLogin = document.getElementById('reg-form-login')
+let regName = document.getElementById('reg-form-name')
+let regPassword = document.getElementById('reg-form-password')
+let loginButton = document.querySelector('.login-form')
+regButton.addEventListener('click', function () {
+  loginUser(regLogin, regName, regPassword)
+})
+loginButton.addEventListener('click', function () {
+  autorizationForm()
+  listenerOfAutoriz()
+})
+}
+
+function listenerOfAutoriz() {
+let loginUserButton = document.querySelector('.login')
+let logLogin = document.querySelector('.log-form-login')
+let logPassword= document.querySelector('.log-form-password')
+let regPageButton= document.querySelector('.reg-form-button')
+logLogin.addEventListener('input', function () {
+  if (logLogin.value.length >= 3 && logPassword.value.length >= 3) {
+    loginUserButton.removeAttribute('disabled')
+  }
+  else{
+    loginUserButton.setAttribute('disabled', 'disabled')
+    }})
+    logPassword.addEventListener('input', function () {
+      if (logLogin.value.length >= 3 && logPassword.value.length >= 3) {
+        loginUserButton.removeAttribute('disabled')
+      }
+      else{
+        loginUserButton.setAttribute('disabled', 'disabled')
+      }
+  })
+  
+    loginUserButton.addEventListener('click', function () {
+      autorisationUser(logLogin, logPassword)
+    })
+    regPageButton.addEventListener('click', function () {
+      renderRegForm()
+      listenerOfReg()
+    })
+  }          
+  export {commentClickListener, initEventListeners, listenerHref}
